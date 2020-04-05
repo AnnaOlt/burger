2485802a52a7d5fe3c8e9db13f5a19de70e53dd7
Anna Olt <anna.olt16@gmail.com>
2020-04-05 19:26:40 -0400

    "changes"

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require("express");
-var PORT = process.env.PORT || 8080;
+const exphbs = require("express-handlebars");
 
-var app = express();
+const PORT = process.env.PORT || 8080;
+
+const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("../controllers/burgers_controllers.js");
+const routes = require("../controllers/burgers_controllers.js");
 
 app.use(routes);
 
